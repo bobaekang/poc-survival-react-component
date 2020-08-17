fca21921c3e272db173ae019c242b27d760204d2
Bobae Kang <bkang.dev@gmail.com>
2020-08-17 13:15:55 -0500

    Create a file for mock data

---
+++ src/mockData.js
@@ -0,0 +1,18 @@
+export const factors = [
+  {
+    label: 'Age',
+    value: 'AGE',
+  },
+  {
+    label: 'Race',
+    value: 'RACE',
+  },
+]
+
+export const filter = {}
+
+export const response = {
+  pval: null,
+  risktable: [],
+  survival: [],
+}
