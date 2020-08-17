fe971a16c595fbf5df2084c77ea572d775195a18
Bobae Kang <bkang.dev@gmail.com>
2020-08-17 13:30:22 -0500

    Set up a simple form for ControlForm

--- src/SurvivalAnalysis/ControlForm.jsx
+++ src/SurvivalAnalysis/ControlForm.jsx
@@ -1,19 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const styles = {
   container: {
     border: '1px solid #999',
     margin: '.5rem',
-    padding: '.5rem',
+    padding: '1rem .5rem',
     textAlign: 'center',
+    width: '100%',
+  },
+  inputContainer: {
+    container: {
+      display: 'flex',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      margin: '.5rem',
+    },
+    label: {
+      margin: '.5rem',
+    },
+    input: {
+      margin: '.5rem',
+      minWidth: '200px',
+    },
+  },
+  field: {
     width: '100%',
   },
 }
 
-const ControlForm = () => {
+const ControlFormSelect = ({ label, options, ...selectAttrs }) => (
+  <div style={styles.inputContainer.container}>
+    <label style={styles.inputContainer.label}>{label}</label>
+    <div style={styles.inputContainer.input}>
+      <select style={styles.field} {...selectAttrs}>
+        <option value="">--Please choose an option--</option>
+        {options.map(({ label, value }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </div>
+  </div>
+)
+
+const ControlForm = ({ factors, onSubmit }) => {
+  const [treatment, setTreatment] = useState('')
+  const [confounder, setConfounder] = useState('')
+
   return (
     <div style={styles.container}>
-      <h1>control form here</h1>
+      <h1>Select factors</h1>
+      <form>
+        <ControlFormSelect
+          label="Treatment"
+          options={factors}
+          onChange={(e) => {
+            if (e.target.value === '') setConfounder('')
+            setTreatment(e.target.value)
+          }}
+          value={treatment}
+        />
+        <ControlFormSelect
+          label="Confounder"
+          options={factors.filter(({ value }) => value !== treatment)}
+          disabled={treatment === ''}
+          onChange={(e) => setConfounder(e.target.value)}
+          value={confounder}
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault()
+            onSubmit({ treatment, confounder })
+          }}
+        >
+          Apply
+        </button>
+      </form>
     </div>
   )
 }
--- src/SurvivalAnalysis/index.jsx
+++ src/SurvivalAnalysis/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ControlForm from './ControlForm'
 import RiskTable from './RiskTable'
 import SurvivalPlot from './SurvivalPlot'
+import { factors } from '../mockData'
 
 const styles = {
   container: {
@@ -22,10 +23,14 @@ const styles = {
 }
 
 const SurvivalAnalysis = () => {
+  const handleSubmit = (userInput) => {
+    console.log('user input', userInput)
+  }
+
   return (
     <div style={styles.container}>
       <div style={styles.columnLeft}>
-        <ControlForm />
+        <ControlForm factors={factors} onSubmit={handleSubmit} />
       </div>
       <div style={styles.columnRight}>
         <SurvivalPlot />
