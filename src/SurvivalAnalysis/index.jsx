dca1518fead6f103b6e16a2b8c4cc85715c577ea
Bobae Kang <bkang.dev@gmail.com>
2020-08-19 15:40:03 -0500

    Use state for survival series to plot

--- src/SurvivalAnalysis/index.jsx
+++ src/SurvivalAnalysis/index.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ControlForm from './ControlForm'
 import RiskTable from './RiskTable'
 import SurvivalPlot from './SurvivalPlot'
 import { factors } from '../mockData'
-import { fetchSurvivalResult } from './utils'
+import { fetchSurvivalResult, getSurvivalSeries } from './utils'
 import styles from './SurvivalAnalysis.module.css'
 
 const SurvivalAnalysis = () => {
+  const [survivalSeries, setSurvivalSeries] = useState([])
   const handleSubmit = (userInput) => {
     console.log('user input', userInput)
-    fetchSurvivalResult(userInput).then((result) =>
+    fetchSurvivalResult(userInput).then((result) => {
       console.log('result', result)
-    )
+      setSurvivalSeries(getSurvivalSeries(result.survival, userInput))
+    })
   }
 
   return (
